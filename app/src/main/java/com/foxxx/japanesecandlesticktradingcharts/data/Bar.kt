e1e3baa0d2a825b754456e6361d0dc055dc72677
Foxxx48 <Foxxdim@mail.ru>
2024-01-31 23:00:36 +0300

    Added TerminalState.kt

--- app/src/main/java/com/foxxx/japanesecandlesticktradingcharts/data/Bar.kt
+++ app/src/main/java/com/foxxx/japanesecandlesticktradingcharts/data/Bar.kt
@@ -1,17 +1,20 @@
 package com.foxxx.japanesecandlesticktradingcharts.data
 
+import android.os.Parcelable
 import androidx.compose.runtime.Immutable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 import java.util.Calendar
 import java.util.Date
 
 @Immutable
+@Parcelize
 data class Bar(@SerializedName("o") val open: Float,
                @SerializedName("c") val close: Float,
                @SerializedName("l") val low: Float,
                @SerializedName("h") val high: Float,
                @SerializedName("t") val time: Long
-) {
+): Parcelable {
 
     val calendar: Calendar
         get() {
